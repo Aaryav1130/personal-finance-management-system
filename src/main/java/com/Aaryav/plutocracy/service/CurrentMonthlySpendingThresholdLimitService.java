835f9a196989018c273a7a740e0037edb6d3c871
Aaryav1130 <146359088+Aaryav1130@users.noreply.github.com>
2026-05-25 20:54:09 +0530

    CurrentMonthlySpendingThresholdLimitService.java

---
+++ src/main/java/com/Aaryav/plutocracy/service/CurrentMonthlySpendingThresholdLimitService.java
@@ -0,0 +1,33 @@
+package com.Aaryav.plutocracy.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.Aaryav.plutocracy.dto.request.MonthlySpendingThresholdLimitRequestDto;
+import com.Aaryav.plutocracy.repository.CurrentMonthlySpendingThresholdLimitRepository;
+import com.Aaryav.plutocracy.security.utility.JwtUtils;
+import com.Aaryav.plutocracy.utils.ResponseUtils;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class CurrentMonthlySpendingThresholdLimitService {
+
+	private final CurrentMonthlySpendingThresholdLimitRepository currentMonthlySpendingThresholdLimitRepository;
+	private final JwtUtils jwtUtils;
+	private final ResponseUtils responseUtils;
+
+	public ResponseEntity<?> update(final MonthlySpendingThresholdLimitRequestDto monthlySpendingThresholdLimitRequest,
+			final String token) {
+		final var currentMonthlySpendingThresholdLimit = currentMonthlySpendingThresholdLimitRepository
+				.findByUserId(jwtUtils.extractUserId(token.replace("Bearer ", ""))).get();
+
+		currentMonthlySpendingThresholdLimit.setIsActive(monthlySpendingThresholdLimitRequest.getActive());
+		currentMonthlySpendingThresholdLimit.setLimitValue(monthlySpendingThresholdLimitRequest.getLimitValue());
+
+		currentMonthlySpendingThresholdLimitRepository.save(currentMonthlySpendingThresholdLimit);
+		return responseUtils.monthlySpendingUpdationSuccessResponse();
+	}
+
+}
