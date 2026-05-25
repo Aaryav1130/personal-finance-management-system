e9d8fa009bf8a4e8110e385add4e672d9c114258
Aaryav1130 <146359088+Aaryav1130@users.noreply.github.com>
2026-05-25 20:24:47 +0530

    added controller endpoints and service layer logic for monthly spending threshold

---
+++ src/main/java/com/Aaryav/plutocracy/repository/SpendingThresholdRecordRepository.java
@@ -0,0 +1,16 @@
+package com.Aaryav.plutocracy.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Aaryav.plutocracy.entity.SpendingThresholdRecord;
+
+@Repository
+public interface SpendingThresholdRecordRepository extends JpaRepository<SpendingThresholdRecord, Integer> {
+
+	List<SpendingThresholdRecord> findByUserId(UUID userId);
+
+}
